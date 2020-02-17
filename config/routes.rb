80797a6c2b2503bb795ee8c434975f3175977ad2
genarocanul <gcanul@detei.com.mx>
2020-02-17 16:24:07 -0600

    commit 2

---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+  resources :categories
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/bienvenida", to: "home#index"
+  
+
+  root to: "home#index"
+  
+  
+  resources :articles do
+    get "user/:user_id", to: "articles#from_author", on: :collection
+  end
+
+  #get "articles", to: "articles#index"
+  #get "articles/new", to: "articles#new", as: :new_articles
+  #get "articles/:id", to: "articles#show"
+  #get "articles/:id/edit", to: "articles#edit"
+  #patch "/articles/:id", to: "articles#update", as: :article
+  #post "articles", to: "articles#create"
+  #delete "articles/:id", to:"articles#destroy"
+
+
+
+
+end
